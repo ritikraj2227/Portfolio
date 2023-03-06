8d654bc652c79b9aa23eabf5435399781d42a0dd
ritikraj2227 <ritikraj025@gmail.com>
2023-03-06 11:44:53 +0530

    made some changes

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-
-var myID = document.getElementById("navbar-brand");
-var nav = document.getElementById("navbar-menu");
-var navitem = document.querySelectorAll(".nav-link");
-var myscrollfunc = function(){
-    var y = window.scrollY;
-     if ( y >= 350 ) {
-         myID.style.display = "block"
-     }else{
-         myID.style.display = "none"
-     }
+var myID = document.getElementById('navbar-brand');
+var nav = document.getElementById('navbar-menu');
+var navitem = document.querySelectorAll('.nav-link');
+var myscrollfunc = function () {
+	var y = window.scrollY;
+	if (y >= 350) {
+		myID.style.display = 'block';
+	} else {
+		myID.style.display = 'none';
+	}
 };
 
- 
-window.addEventListener("scroll", myscrollfunc);
+window.addEventListener('scroll', myscrollfunc);
 
-
-
-var navcolor = function(){
-    var y = window.scrollY;
-     if ( y >= 550 ) {
-         nav.style.backgroundColor = "#002447"
-         
-     }  else{
-         nav.style.backgroundColor = "#002447"
-      }
+var navcolor = function () {
+	var y = window.scrollY;
+	if (y >= 550) {
+		nav.style.backgroundColor = '#002447';
+	} else {
+		nav.style.backgroundColor = '#002447';
+	}
 };
 
- 
-window.addEventListener("scroll", navcolor);
+window.addEventListener('scroll', navcolor);
 
-
+var typing = new Typed('.text', {
+	strings: ['Ritik Raj.', 'Web Developer.', 'Web Designer'],
+	typeSpeed: 100,
+	backSpeed: 40,
+	loop: true,
+});
